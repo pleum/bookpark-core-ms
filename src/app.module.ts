ae540f4a66feb2634279a63a4872e95932905edd
chacree <chacree@wongnai.com>
2020-02-20 22:56:48 +0700

    :tada: Initial project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LineModule } from './line/line.module';
+
+@Module({
+  imports: [LineModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
